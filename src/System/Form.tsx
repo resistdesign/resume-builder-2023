1d5bd5f8e7cca285b7788c7197e0c641c0d7d4f4
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-20 23:01:00 -0400

    feat: (System) Add the basis for Form.

---
+++ src/System/Form.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, FC, PropsWithChildren, useCallback } from 'react';
+
+export type FormProps<ValueType extends Record<any, any>> = PropsWithChildren & {
+  name: string;
+  value?: ValueType;
+  onChange?: (name: string, value: ValueType) => void;
+  onSubmit?: (name: string, value: ValueType) => void;
+};
+
+export const Form: FC<FormProps<any>> = <ValueType extends Record<any, any>>({
+  name,
+  value,
+  onChange,
+  onSubmit,
+  children,
+}: FormProps<ValueType>) => {
+  const onChangeInternal = useCallback(
+    (event: ChangeEvent<HTMLFormElement>) => {
+      if (onChange) {
+        const { target } = event;
+        const { value: newValue } = target;
+
+        onChange(name, newValue);
+      }
+    },
+    [onChange, name]
+  );
+  const onSubmitInternal = useCallback(
+    (event: ChangeEvent<HTMLFormElement>) => {
+      if (onSubmit) {
+        const { target } = event;
+        const { value: newValue } = target;
+
+        event.preventDefault();
+        onSubmit(name, newValue);
+      }
+    },
+    [onSubmit, name]
+  );
+
+  return (
+    <form onChange={onChangeInternal} onSubmit={onSubmitInternal}>
+      {children}
+    </form>
+  );
+};
