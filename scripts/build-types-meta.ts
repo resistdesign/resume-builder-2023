e35d32d2b98e6ea5d3bacd041d1b8b6a1aa935d5
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-22 14:41:45 -0400

    chore: (repo) Automatic commit.

--- scripts/build-types-meta.ts
+++ scripts/build-types-meta.ts
@@ -1,8 +1,23 @@
 import FS from 'fs';
 import Path from 'path';
+import { convertTypeScriptToTypeStructureMap } from '../src/System/TypeParsing/TypeParsing';
+import { TypeStructure, TypeStructureMap } from '../src/System/TypeParsing/TypeUtils';
 
 const TYPES_DIRECTORY_PATH = Path.join(__dirname, '..', 'src', 'Types');
 const OUTPUT_JSON_PATH = Path.join(__dirname, '..', 'src', 'Meta', 'TypeStructureMap.json');
 const TYPE_FILE_LIST = FS.readdirSync(TYPES_DIRECTORY_PATH);
 
-console.log(TYPE_FILE_LIST);
+let TYPE_STRUCTURE_MAP: Record<string, TypeStructure> = {};
+
+for (const typeFileName of TYPE_FILE_LIST) {
+  const typeFilePath = Path.join(TYPES_DIRECTORY_PATH, typeFileName);
+  const typeFileContent = FS.readFileSync(typeFilePath, 'utf8');
+  const currentTypeStructureMap: TypeStructureMap = convertTypeScriptToTypeStructureMap(typeFileContent);
+
+  TYPE_STRUCTURE_MAP = {
+    ...TYPE_STRUCTURE_MAP,
+    ...currentTypeStructureMap,
+  };
+}
+
+FS.writeFileSync(OUTPUT_JSON_PATH, JSON.stringify(TYPE_STRUCTURE_MAP, null, 2));
