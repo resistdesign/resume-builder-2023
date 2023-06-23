0b2a2fd2a6f615d2fa8d892a4f56bcbe0c7e991f
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-22 23:03:55 -0400

    chore: (repo) Automatic commit.

--- src/System/InputGeneration.tsx
+++ src/System/InputGeneration.tsx
@@ -1,27 +1,40 @@
-import {TypeStructure, TypeStructureMap} from "./TypeParsing/TypeUtils";
+import {getTypeStructureWithFilteredContent, TypeStructure, TypeStructureMap} from './TypeParsing/TypeUtils';
+import {Input} from './Input';
 
-export const TYPE_TO_INPUT_TYPE_MAP : Record<string, string> = {
-    "string": "text",
-    "number": "number",
-    "boolean": "checkbox",
-    "Date": "date",
-    "any": "text",
-    "object": "text",
-    "array": "text",
+export const TYPE_TO_INPUT_TYPE_MAP: Record<string, string> = {
+    string: 'text',
+    number: 'number',
+    boolean: 'checkbox',
+    Date: 'date',
+    any: 'text',
+    object: 'text',
+    array: 'text',
 };
 
 export const convertTypeStructureToInputs = (
+    name: string,
     typeStructure: TypeStructure,
     typeStructureMap: TypeStructureMap
 ) => {
+    const {contentNames} = typeStructure;
     const {
         type: typeStructureType,
-    } = typeStructure;
+        multiple: typeStructureMultiple,
+        content = [],
+    } = getTypeStructureWithFilteredContent(contentNames, typeStructure);
     const typeIsPrimitive = !(typeStructureType in typeStructureMap);
+    const inputType = TYPE_TO_INPUT_TYPE_MAP[typeStructureType];
 
-    if (typeIsPrimitive) {
-
-    }else{
-
+    if (typeStructureMultiple) {
+        // TODO: Need a list component.
+    } else {
+        if (typeIsPrimitive) {
+            return <Input key={name} name={name} type={inputType}/>;
+        } else {
+            return content.map((tS) => {
+                const {name: tSName,} = tS;
+                return convertTypeStructureToInputs(tSName, tS, typeStructureMap);
+            };
+        }
     }
 };
