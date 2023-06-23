046d705002e640a63ccb2f4959f0ded1ac05a576
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-23 13:30:35 -0400

    feat: (System) Allow primitive values in List.

--- src/System/List.tsx
+++ src/System/List.tsx
@@ -1,26 +1,30 @@
 import React, { FC } from 'react';
 
+export const getCleanPrimitiveStringValue = (value: any): string =>
+  value !== undefined && value !== null ? `${value}` : '';
+
 export const getItemName = <ValueType extends Record<any, any>>(
   item: ValueType = {} as any,
   itemNameTemplate: string = ''
-): string =>
-  itemNameTemplate.replace(/\`(\w+)\`/g, (match, key) =>
-    item[key] !== undefined && item[key] !== null ? `${item[key]}` : ''
-  );
+): string => itemNameTemplate.replace(/\`(\w+)\`/g, (match, key) => getCleanPrimitiveStringValue(item[key]));
 
-export type ListProps<ValueType extends Record<any, any>> = {
-  items: ValueType[];
+export type ListProps = {
+  items: any[];
   itemNameTemplate?: string;
+  itemsArePrimitive?: boolean;
 };
 
-export const List: FC<ListProps<any>> = <ValueType extends Record<any, any>>({
-  items = [],
-  itemNameTemplate = '',
-}: ListProps<ValueType>) => {
+export const List: FC<ListProps> = ({ items = [], itemNameTemplate = '', itemsArePrimitive = false }: ListProps) => {
+  // TODO: Edit/View/Details/Delete buttons.
+  // TODO: Reordering.
   return (
     <ul>
       {items.map((item, index) => {
-        return <li key={index}>{getItemName(item, itemNameTemplate)}</li>;
+        return (
+          <li key={index}>
+            {itemsArePrimitive ? getCleanPrimitiveStringValue(item) : getItemName(item, itemNameTemplate)}
+          </li>
+        );
       })}
       <li>
         <button>More</button>
