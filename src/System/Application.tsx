f3524482ec8e512342b36853499b03903286472b
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-26 17:10:59 -0400

    chore: (repo) Automatic commit.

--- src/System/Application.tsx
+++ src/System/Application.tsx
@@ -1,5 +1,10 @@
-import React, { FC, PropsWithChildren } from 'react';
-import { TypeStructureMap } from './TypeParsing/TypeUtils';
+import React, { FC, PropsWithChildren, useCallback, useMemo, useState } from 'react';
+import { TypeStructure, TypeStructureMap } from './TypeParsing/TypeUtils';
+
+export type NavigationItem = {
+  value: any;
+  typeStructure: TypeStructure;
+};
 
 export type ApplicationProps<TypeStructureMapType extends TypeStructureMap> = PropsWithChildren & {
   typeStructureMap: TypeStructureMapType;
@@ -9,5 +14,19 @@ export type ApplicationProps<TypeStructureMapType extends TypeStructureMap> = Pr
 };
 
 export const Application: FC<ApplicationProps<any>> = ({ typeStructureMap, value, entryType, onChange, children }) => {
+  const typeStructure = useMemo(() => typeStructureMap[entryType], [typeStructureMap, entryType]);
+  const initialNavigationItem: NavigationItem = useMemo(() => ({ value, typeStructure }), [value, typeStructure]);
+  const [nav, setNav] = useState<NavigationItem[]>([initialNavigationItem]);
+  const currentNav = useMemo(() => nav[nav.length - 1], [nav]);
+  const onNavTo = useCallback(
+    (navItem: NavigationItem) => {
+      setNav([...nav, navItem]);
+    },
+    [nav]
+  );
+  const onNavBack = useCallback(() => {
+    setNav(nav.slice(0, nav.length - 1));
+  }, [nav]);
+
   return <div>{children}</div>;
 };
