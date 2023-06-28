930f4944aa40b6bffbd21427e9099af0bebe1762
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-27 20:07:35 -0400

    feat: (System) Add item reordering to List.

--- src/System/List.tsx
+++ src/System/List.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren, useCallback, useMemo } from 'react';
+import React, { FC, PropsWithChildren, useCallback, useMemo, useState } from 'react';
 import { getDefaultItemForTypeStructure, getTypeStructureIsPrimitive, TypeStructure } from './TypeParsing/TypeUtils';
 import { TAG_TYPES } from './TypeStructureComponent';
 
@@ -38,6 +38,8 @@ export const List: FC<ListProps> = ({
   onChange,
   onNavigateToPath,
 }: ListProps) => {
+  const [selectedIndices, setSelectedIndices] = useState<any[]>([]);
+  const [itemsAreMoving, setItemsAreMoving] = useState(false);
   const itemNameTemplate = useMemo(() => {
     const { tags: { [TAG_TYPES.itemName]: { value = '' } = {} } = {} } = typeStructure;
 
@@ -73,13 +75,59 @@ export const List: FC<ListProps> = ({
     },
     [items, onChangeInternal]
   );
-  // TODO: Reordering.
+  const onToggleItemSelected = useCallback(
+    (index: number) => {
+      if (selectedIndices.includes(index)) {
+        setSelectedIndices(selectedIndices.filter((ind) => ind !== index));
+      } else {
+        setSelectedIndices([...selectedIndices, index]);
+      }
+    },
+    [selectedIndices, setSelectedIndices]
+  );
+  const onDeselectAllItems = useCallback(() => {
+    setSelectedIndices([]);
+  }, [setSelectedIndices]);
+  const onSetItemsAreMoving = useCallback(() => {
+    setItemsAreMoving(true);
+  }, [setItemsAreMoving]);
+  const onCleanupMovingItems = useCallback(() => {
+    setItemsAreMoving(false);
+    setSelectedIndices([]);
+  }, [setItemsAreMoving, setSelectedIndices]);
+  const onMoveItems = useCallback(
+    (moveItemsToIndex: number) => {
+      const itemsWithoutSelected = [...items].filter((_itm, ind) => !selectedIndices.includes(ind));
+      const selectedItems = [...items].filter((_itm, ind) => selectedIndices.includes(ind));
+      const targetItem = items[moveItemsToIndex];
+      const newTargetIndex = itemsWithoutSelected.indexOf(targetItem);
+      const newItems = [
+        ...itemsWithoutSelected.slice(0, newTargetIndex),
+        ...selectedItems,
+        ...itemsWithoutSelected.slice(newTargetIndex),
+      ];
+
+      onChangeInternal(newItems);
+
+      onCleanupMovingItems();
+    },
+    [items, onChangeInternal, onCleanupMovingItems, selectedIndices]
+  );
 
   return (
     <ul>
       {items.map((item, index) => {
-        return (
+        return itemsAreMoving && selectedIndices.includes(index) ? undefined : (
           <li key={index}>
+            {itemsAreMoving ? (
+              <SelectItemButton index={index} onSelectItem={onMoveItems}>
+                Move Here
+              </SelectItemButton>
+            ) : (
+              <SelectItemButton index={index} onSelectItem={onToggleItemSelected}>
+                Select
+              </SelectItemButton>
+            )}
             {getItemLabel(item)}
             <SelectItemButton index={index} onSelectItem={onOpenItem}>
               Open
@@ -93,6 +141,23 @@ export const List: FC<ListProps> = ({
       <li>
         <button onClick={onAddItem}>+ Add Item</button>
       </li>
+      {selectedIndices.length > 0 ? (
+        <li>
+          <button onClick={onSetItemsAreMoving}>Move Item(s)</button>
+        </li>
+      ) : undefined}
+      {itemsAreMoving ? (
+        <>
+          <li>
+            <SelectItemButton index={items.length} onSelectItem={onMoveItems}>
+              Move Here
+            </SelectItemButton>
+          </li>
+          <li>
+            <button onClick={onDeselectAllItems}>Cancel</button>
+          </li>
+        </>
+      ) : undefined}
     </ul>
   );
 };
