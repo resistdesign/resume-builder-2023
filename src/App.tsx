13e96c142941650c778237be98c6669b2af72ebe
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-27 20:39:18 -0400

    chore: (repo) Automatic commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import '@picocss/pico/css/pico.min.css';
 import React, { FC, useState } from 'react';
-import { Form } from './System/Form';
-import { Input } from './System/Input';
+import TSM from '././Meta/TypeStructureMap.json';
+import { Application } from './System/Application';
 
 // TODO: i18n.
 
 export const App: FC = () => {
-  const [person, setPerson] = useState({});
+  const [resume, setResume] = useState({});
 
-  return (
-    <Form name={'person'} value={person} onSubmit={setPerson}>
-      <Input name="description" />
-      <Input name="email" />
-      <button type="submit">Done</button>
-    </Form>
-  );
+  return <Application typeStructureMap={TSM} value={resume} entryType="Resume" onChange={setResume} />;
 };
