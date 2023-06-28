70d83125dc16544362d93dad1f2a27bd2071b1c6
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-28 10:43:33 -0400

    chore: (repo) Automatic commit.

--- src/Types/Education.ts
+++ src/Types/Education.ts
@@ -1,7 +1,36 @@
 export type Education = {
+  /**
+   * @label Establishment
+   */
   establishment: string;
+  /**
+   * @label Program
+   */
   program: string;
-  startDate: string;
-  endDate: string;
+  /**
+   * @label Start Date
+   */
+  startDate: Date;
+  /**
+   * @label End Date
+   */
+  endDate: Date;
+  /**
+   * @label Credential Type
+   * @options
+   *  Individual Courses
+   *  Certificate
+   *  Associate
+   *  Bachelors
+   *  Masters
+   *  Doctorate
+   *  Professional
+   *  PHD
+   * @allowCustomValue
+   * */
+  credentialType: string;
+  /**
+   * @label Achievements
+   */
   achievements: string[];
 };
