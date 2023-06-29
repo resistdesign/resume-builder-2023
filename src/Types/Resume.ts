65f8358778678fcaeb2bd94e1a6e903198765364
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-28 20:53:32 -0400

    chore: (repo) Automatic commit.

--- src/Types/Resume.ts
+++ src/Types/Resume.ts
@@ -3,13 +3,28 @@ import { Employment } from './Employment';
 import { Education } from './Education';
 
 export type Resume = {
+  /**
+   * @label Subject
+   */
   subject: Person;
   /**
    * @label Objective
    * */
   objective: string;
+  /**
+   * @label Date
+   * */
   date: Date;
+  /**
+   * @label Employment
+   * */
   employment: Employment[];
+  /**
+   * @label Education
+   * */
   education: Education[];
+  /**
+   * @label References
+   * */
   references: Person[];
 };
