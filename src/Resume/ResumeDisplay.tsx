b57bbbbffd451fcc786c6a97e3709aa4d6f5528d
Ryanne Graff <ryantgraff@resistdesign.com>
2023-06-30 12:00:50 -0400

    chore: (repo) Automatic commit.

--- src/Resume/ResumeDisplay.tsx
+++ src/Resume/ResumeDisplay.tsx
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
 import { Display } from '../System/Display';
 
-export const ResumeDisplay = styled(Display)``;
+export const ResumeDisplay = styled(Display)`
+  display: grid;
+  grid-template:
+   "subject subject subject date" auto
+   "objective objective objective objective" auto
+   "employment employment education education" auto
+   "references references references references" auto;
+  gap: 1em;
+
+  border: 1px solid #ccc;
+
+  & div {
+    border: 1px solid #ccc;
+  }
+`;
