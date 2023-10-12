59da373d70e2ac5464111b0811f9097c0d012c6b
Ryanne Graff <ryantgraff@resistdesign.com>
2023-10-12 12:52:36 -0400

    chore: (repo) Automatic commit.

--- src/Resume/Layout/ResumeLayout.tsx
+++ src/Resume/Layout/ResumeLayout.tsx
@@ -4,10 +4,27 @@ import { Resume } from '../../Types/Resume';
 
 export const ResumeBase = styled.div`
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-template-rows: repeat(8, 1fr);
-  grid-column-gap: 16px;
-  grid-row-gap: 16px;
+  grid-template-columns: repeat(6, 1.125in);
+  grid-auto-rows: 1.125in;
+  gap: 0.25in;
+  padding: 0.25in;
+  width: 8.5in;
+`;
+export const DemoPanel = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  background-color: lightgray;
+  border-radius: 0.5em;
+`;
+export const Header = styled(DemoPanel)`
+  grid-column: 1 / 7;
+  grid-row: 1 / 2;
+  background-color: red;
+`;
+export const DisplayArea = styled(DemoPanel)`
+  grid-column: 1 / 5;
+  grid-row: 2 / 9;
+  background-color: yellow;
 `;
 
 export type ResumeProps = {
@@ -15,5 +32,10 @@ export type ResumeProps = {
 };
 
 export const ResumeLayout: FC<ResumeProps> = ({ resume = {} as Resume }) => {
-  return <ResumeBase></ResumeBase>;
+  return (
+    <ResumeBase>
+      <Header>Header</Header>
+      <DisplayArea>Display Area</DisplayArea>
+    </ResumeBase>
+  );
 };
