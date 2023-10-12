d55cf1cd0b21e477b7381ab2f8a84c4e7da94f47
Ryanne Graff <ryantgraff@resistdesign.com>
2023-10-12 13:19:30 -0400

    chore: (repo) Automatic commit.

--- src/Resume/Layout/ResumeLayout.tsx
+++ src/Resume/Layout/ResumeLayout.tsx
@@ -15,6 +15,7 @@ export const DemoPanel = styled.div`
   grid-row: 1 / 2;
   background-color: lightgray;
   border-radius: 0.5em;
+  padding: 0.25in;
 `;
 export const Header = styled(DemoPanel)`
   grid-column: 1 / 7;
@@ -25,10 +26,16 @@ export const DisplayArea = styled(DemoPanel)`
   grid-column: 1 / 5;
   grid-row: 2 / 9;
   background-color: yellow;
-`;export const Footer = styled(DemoPanel)`
+`;
+export const SideArea = styled(DemoPanel)`
+  grid-column: 5 / 7;
+  grid-row: 2 / 9;
+  background-color: lime;
+`;
+export const Footer = styled(DemoPanel)`
   grid-column: 1 / 7;
   grid-row: 9 / 10;
-  background-color: green;
+  background-color: purple;
 `;
 
 export type ResumeProps = {
@@ -40,6 +47,7 @@ export const ResumeLayout: FC<ResumeProps> = ({ resume = {} as Resume }) => {
     <ResumeBase>
       <Header>Header</Header>
       <DisplayArea>Display Area</DisplayArea>
+      <SideArea>Side Area</SideArea>
       <Footer>Footer</Footer>
     </ResumeBase>
   );
