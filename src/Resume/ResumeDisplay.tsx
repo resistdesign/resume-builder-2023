5a0d52a039160fb5b732ea3733460bc865cc8706
Ryanne Graff <ryantgraff@resistdesign.com>
2023-11-08 13:03:01 -0500

    chore: (repo) Automatic commit.

--- src/Resume/ResumeDisplay.tsx
+++ src/Resume/ResumeDisplay.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Resume } from '../Types/Resume';
 import styled from 'styled-components';
 import { ResumeLayout } from './Layout/ResumeLayout';
+import {convertLayoutToCSS} from "../System/LayoutUtils/EasyLayout";
 
 type FormattedDateParts = {
   year: string | number;
@@ -52,3 +53,9 @@ export const ResumeDisplay: FC<ResumeDisplayProps> = ({ resume = {} as Resume })
     </ResumeDisplayBase>
   );
 };
+
+console.log(convertLayoutToCSS(`
+header header, 2fr
+side body, 5fr
+footer footer, 1fr
+`));
--- src/System/LayoutUtils/EasyLayout.tsx
+++ src/System/LayoutUtils/EasyLayout.tsx
@@ -2,40 +2,37 @@ import { FC } from 'react';
 
 export type ComponentMap = Record<string, FC>;
 
-const convertLayoutToCSS = (layout: string = ''): string => {
+export const convertLayoutToCSS = (layout: string = ''): string => {
   const lines = layout.split('\n');
+  const areas = [];
+  const rows = [];
 
   let css = '';
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
-    const areas = [];
-    const rows = [];
 
     if (i === lines.length - 1) {
       // Column Widths
-      css += `grid-template-columns: ${line};`;
+      css += `\ngrid-template-columns: ${line};`;
     } else {
       const parts = line.split(',');
 
       areas.push(parts[0]);
       rows.push(parts[1]);
     }
-
-    css += `grid-template-areas: ${areas.filter((a) => !!(a && a.trim())).join(' ')};`;
-    css += `grid-template-rows: ${rows.filter((r) => !!(r && r.trim())).join(' ')};`;
   }
+
+  css += `\ngrid-template-areas: ${areas.filter((a) => !!(a && a.trim())).join(' ')};`;
+  css += `\ngrid-template-rows: ${rows.filter((r) => !!(r && r.trim())).join(' ')};`;
 
   return css;
 };
 
-const getLayoutAreas = (layout: string = ''): string[] => {};
+// TODO: const getLayoutAreas = (layout: string = ''): string[] => {};
 
 export const getLayoutComponents = (layout: string = ''): ComponentMap => {
   let compMap: ComponentMap = {};
 
-  const;
-
-  return;
-  compMap;
+  return compMap;
 };
