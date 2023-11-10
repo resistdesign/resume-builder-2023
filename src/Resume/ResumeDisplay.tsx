e7a1f779a691a14d3cca46989e95cc0479e3675e
Ryanne Graff <ryantgraff@resistdesign.com>
2023-11-09 22:35:29 -0500

    chore: (repo) Automatic commit.

--- src/Resume/ResumeDisplay.tsx
+++ src/Resume/ResumeDisplay.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Resume } from '../Types/Resume';
 import styled from 'styled-components';
-import { getLayoutComponents } from '../System/LayoutUtils/EasyLayout';
+import { ResumeLayout } from './Layout/ResumeLayout';
 
 type FormattedDateParts = {
   year: string | number;
@@ -45,66 +45,10 @@ export type ResumeDisplayProps = {
   resume?: Resume;
 };
 
-const {
-  layout: Layout,
-  areas: { Header, Side, Body, Footer },
-} = getLayoutComponents(
-  ResumeDisplayBase,
-  styled.div`
-    background-color: blue;
-  `
-)`
-header header, 2fr
-side body, 5fr
-footer footer, 1fr
-\\ 1fr 6fr
-`;
-const {
-  layout: BodyLayout,
-  areas: { BodyTop, BodyMiddle, BodyBottom },
-} = getLayoutComponents(
-  styled(Body)`
-    gap: 1em;
-  `,
-  styled.div`
-    background-color: red;
-  `
-)`
-body-top, 1fr
-body-middle, 3fr
-body-bottom, 1fr
-\\ 1fr
-`;
-const {
-  layout: BodyMiddleLayout,
-  areas: { BodyMiddleLeft, BodyMiddleCenter, BodyMiddleRight },
-} = getLayoutComponents(
-  styled(BodyMiddle)`
-    gap: 1em;
-  `,
-  styled.div`
-    background-color: green;
-  `
-)`
-body-middle-left body-middle-center body-middle-right, 1fr
-\\ 1fr 3fr 1fr
-`;
-
 export const ResumeDisplay: FC<ResumeDisplayProps> = ({ resume = {} as Resume }) => {
   return (
-    <Layout>
-      <Header>Header</Header>
-      <Side>Side</Side>
-      <BodyLayout>
-        <BodyTop>Body Top</BodyTop>
-        <BodyMiddleLayout>
-          <BodyMiddleLeft>Body Middle Left</BodyMiddleLeft>
-          <BodyMiddleCenter>Body Middle Center</BodyMiddleCenter>
-          <BodyMiddleRight>Body Middle Right</BodyMiddleRight>
-        </BodyMiddleLayout>
-        <BodyBottom>Body Bottom</BodyBottom>
-      </BodyLayout>
-      <Footer>Footer</Footer>
-    </Layout>
+    <ResumeDisplayBase>
+      <ResumeLayout resume={resume} />
+    </ResumeDisplayBase>
   );
 };
